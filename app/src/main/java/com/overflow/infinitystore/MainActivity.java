d2aa554190815994b19edf9da429a440f342c1b1
liorspencer <juliodiversity@gmail.com>
2024-10-22 22:08:40 -0300

    inicio da tela menu

--- app/src/main/java/com/overflow/infinitystore/MainActivity.java
+++ app/src/main/java/com/overflow/infinitystore/MainActivity.java
@@ -1,14 +1,22 @@
 package com.overflow.infinitystore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    protected Button btnLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnLogar = findViewById(R.id.buttonLogar);
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), TelaMenu.class);
+                resultado.launch(intent);
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> resultado = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                Intent intent = result.getData();
+                String codigo = intent.getStringExtra("codigo");
+                if (codigo.equals("1")) {
+                    intent.getSerializableExtra("cliente");
+                }
+                /*
+                if (codigo.equals("2")) {
+                    produtos.add((Produto) intent.getSerializableExtra("produto"));
+                }
+                if (codigo.equals("3")) {
+                    vendas.add((Venda) intent.getSerializableExtra("venda"));
+                }*/
+            }
+        }
+    });
 }
